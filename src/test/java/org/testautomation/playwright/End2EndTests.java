3f287aaa4e1f973b2f87281ecf159f76bb69b903
Olha Haichenkova <olha@liquibase.com>
2025-05-12 09:30:41 +0300

    Added more end-to-end tests

--- src/test/java/org/testautomation/playwright/End2EndTests.java
+++ src/test/java/org/testautomation/playwright/End2EndTests.java
@@ -45,4 +45,134 @@ public class End2EndTests extends AbstractTest {
     assertThat(updatedCostText).isEqualTo("$97,048.22");
   }
 
+  @Test
+  public void verifyKubernetesEngineConfigurationsSet(Page page) {
+
+    CalculatorPage calculator = new CalculatorPage(page);
+    ServiceType serviceType = ServiceType.GKE;
+
+    calculator.addToEstimate(serviceType.getProduct());
+    GKEServiceBuilder builder = ServiceConfigurationBuilderFactory.getBuilder(serviceType);
+
+    Service service = builder.numberOfNodes(5)
+        .operatingSystem(OperationSystem.FREE_CONTAINER_OPTIMIZED)
+        .machineType(MachineType.C2D_STANDARD_32)
+        .region(Region.FRANKFURT)
+        .committedUse(CommittedUse.THREE_YEARS)
+        .build();
+
+    ServiceConfigurationComponent activeService = calculator.getActiveService();
+    activeService.fillInCalculationForm(service);
+
+    String updatedCostText = calculator.getCostDetails().readTotalCost();
+    assertThat(updatedCostText).isEqualTo("$3,153.18");
+  }
+
+  @Test
+  public void verifyKubernetesEngineAndComputeEngineConfigurationsSet(Page page) {
+
+    CalculatorPage calculator = new CalculatorPage(page);
+    ServiceType serviceType = ServiceType.GKE;
+
+    calculator.addToEstimate(serviceType.getProduct());
+    GKEServiceBuilder builder = ServiceConfigurationBuilderFactory.getBuilder(serviceType);
+
+    Service service = builder.numberOfNodes(5)
+        .operatingSystem(OperationSystem.FREE_UBUNTU)
+        .machineType(MachineType.M1_ULTRAMEM_80)
+        .region(Region.TORONTO)
+        .committedUse(CommittedUse.ONE_YEAR)
+        .build();
+
+    ServiceConfigurationComponent activeService = calculator.getActiveService();
+    activeService.fillInCalculationForm(service);
+
+    CostDetailsComponent costDetails = calculator.getCostDetails();
+    String kubernetesServiceCost = costDetails.readActiveServiceCost();
+    assertThat(kubernetesServiceCost).isEqualTo("$29,983.99");
+
+    ServiceType secondServiceType = ServiceType.INSTANCES;
+
+    calculator.addToEstimate(secondServiceType.getProduct());
+    InstancesServiceBuilder secondBuilder = ServiceConfigurationBuilderFactory.getBuilder(secondServiceType);
+
+    service = secondBuilder.numberOfInstances(1)
+        .operatingSystem(OperationSystem.FREE)
+        .machineType(MachineType.M1_ULTRAMEM_160)
+        .region(Region.LOS_ANGELES)
+        .committedUse(CommittedUse.ONE_YEAR)
+        .build();
+
+    activeService = calculator.getActiveService();
+    activeService.fillInCalculationForm(service);
+
+    String engineServiceCost = costDetails.readActiveServiceCost();
+    assertThat(engineServiceCost).isEqualTo("$13,050.63");
+
+    String updatedCostText = calculator.getCostDetails().readTotalCost();
+    assertThat(updatedCostText).isEqualTo("$43,034.61");
+  }
+
+  @Test
+  public void verifyTwoComputeEngineInstancesConfigurationsSet(Page page) {
+
+    CalculatorPage calculator = new CalculatorPage(page);
+    ServiceType serviceType = ServiceType.INSTANCES;
+
+    calculator.addToEstimate(serviceType.getProduct());
+    InstancesServiceBuilder builder = ServiceConfigurationBuilderFactory.getBuilder(serviceType);
+
+    Service service = builder.numberOfInstances(1)
+        .operatingSystem(OperationSystem.FREE)
+        .machineType(MachineType.C2D_STANDARD_16)
+        .region(Region.FRANKFURT)
+        .committedUse(CommittedUse.ONE_YEAR)
+        .build();
+
+    ServiceConfigurationComponent activeService = calculator.getActiveService();
+    activeService.fillInCalculationForm(service);
+
+    CostDetailsComponent costDetails = calculator.getCostDetails();
+    String firstEngineServiceCost = costDetails.readActiveServiceCost();
+    assertThat(firstEngineServiceCost).isEqualTo("$431.58");
+
+    calculator.addToEstimate(serviceType.getProduct());
+    Service secondService = builder.numberOfInstances(1)
+        .operatingSystem(OperationSystem.FREE)
+        .machineType(MachineType.M2_MEGAMEM_416)
+        .region(Region.LONDON)
+        .committedUse(CommittedUse.THREE_YEARS)
+        .build();
+
+    activeService = calculator.getActiveService();
+    activeService.fillInCalculationForm(secondService);
+
+    String secondEngineServiceCost = costDetails.readActiveServiceCost();
+    assertThat(secondEngineServiceCost).isEqualTo("$15,985.21");
+
+    String updatedCostText = calculator.getCostDetails().readTotalCost();
+    assertThat(updatedCostText).isEqualTo("$16,416.79");
+  }
+
+  @Test
+  public void verifySoleTenantNodesConfigurationsSet(Page page) {
+
+    CalculatorPage calculator = new CalculatorPage(page);
+    ServiceType serviceType = ServiceType.SOLE_TENANT_NODES;
+
+    calculator.addToEstimate(serviceType.getProduct());
+
+    SoleTenantNodesServiceBuilder builder = ServiceConfigurationBuilderFactory.getBuilder(serviceType);
+    SoleTenantNodesService service = builder.serviceType(serviceType)
+        .region(Region.TOKYO)
+        .numberOfNodes(3)
+        .build();
+
+    ServiceConfigurationComponent activeService = calculator.getActiveService();
+    activeService.fillInCalculationForm(service);
+
+    String updatedCostText = calculator.getCostDetails().readTotalCost();
+    assertThat(updatedCostText).isEqualTo("$17,539.41");
+  }
+
 }
